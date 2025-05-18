d8d108df3fea48e5003c72beb72e10c361c840a4
A1ex-Fu <alex.aj.fu@gmail.com>
2025-05-17 18:59:56 -0700

    another

--- profile.py
+++ profile.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 #
 # CloudLab profile: 4 nodes on a shared LAN running vrpaxos.
-# Compatible with Python 2.7 (no f‑strings, no type hints).
+# Compatible with Python 2.7 (no f-strings, no non-ASCII chars).
 #
 import geni.portal as portal
 import geni.rspec.pg as pg
@@ -8,26 +9,26 @@ import geni.rspec.pg as pg
 pc      = portal.Context()
 request = pc.makeRequestRSpec()
 
-# ---------- Parameters ----------
+# ---------- parameters ----------
 pc.defineParameter("phystype",  "Physical node type",
                    portal.ParameterType.STRING, "",
                    longDescription="Leave blank for any available type.")
 
 pc.defineParameter("linkSpeed", "LAN link speed",
                    portal.ParameterType.INTEGER, 1000000,
-                   [(100000,  "100 Mb/s"),
-                    (1000000, "1 Gb/s"),
-                    (10000000,"10 Gb/s")])
+                   [(100000,  "100Mb/s"),
+                    (1000000, "1Gb/s"),
+                    (10000000,"10Gb/s")])
 
 params = pc.bindParameters()
 
-# ---------- Basic constants ----------
-NODE_COUNT   = 4
-BASE_IP      = "10.10.1."
-PORT_START   = 8080
-DISK_IMAGE   = ("urn:publicid:IDN+emulab.net+image+emulab-ops//"
-                "UBUNTU22-64-STD")
-GIT_URL      = "https://github.com/A1ex-Fu/vrpaxos.git"
+# ---------- constants ----------
+NODE_COUNT = 4
+BASE_IP    = "10.10.1."
+PORT_START = 8080
+DISK_IMAGE = ("urn:publicid:IDN+emulab.net+image+emulab-ops//"
+              "UBUNTU22-64-STD")
+GIT_URL    = "https://github.com/A1ex-Fu/vrpaxos.git"
 
 # ---------- LAN ----------
 lan = request.LAN("lan0")
@@ -36,33 +37,30 @@ lan.bandwidth = params.linkSpeed
 nodes = []
 ips   = []
 
-# ---------- Create nodes ----------
+# ---------- create nodes ----------
 for i in range(NODE_COUNT):
     node = request.RawPC("node%d" % i)
     node.disk_image = DISK_IMAGE
     if params.phystype:
         node.hardware_type = params.phystype
 
-    # interface + IP
     iface = node.addInterface("eth1")
-    ip    = "%s%d" % (BASE_IP, i + 2)   # 10.10.1.2, .3, …
+    ip    = "%s%d" % (BASE_IP, i + 2)  # 10.10.1.2, .3, ...
     iface.addAddress(pg.IPv4Address(ip, "255.255.255.0"))
     lan.addInterface(iface)
 
     nodes.append(node)
     ips.append(ip)
 
-# ---------- Build testConfig2.txt ----------
+# ---------- build testConfig2.txt ----------
 config_lines = ["f 0"]
 for idx, ip in enumerate(ips):
     config_lines.append("replica %s:%d" % (ip, PORT_START + idx))
 config_text = "\\n".join(config_lines)
 
-# ---------- Common bootstrap for each node ----------
-clone_cmd   = "git clone {} ~/vrpaxos".format(GIT_URL)
-config_cmd  = "echo -e \"{}\" > ~/testConfig2.txt".format(config_text)
+clone_cmd  = "git clone %s ~/vrpaxos" % GIT_URL
+config_cmd = "echo -e \"%s\" > ~/testConfig2.txt" % config_text
 
-# Node‑specific run commands
 run_cmds = [
     "cd ~/vrpaxos && ./bench/replica -c ~/testConfig2.txt -i 0 -m vr",
     "cd ~/vrpaxos && ./bench/replica -c ~/testConfig2.txt -i 1 -w -m vr",
@@ -75,5 +73,5 @@ for idx, node in enumerate(nodes):
     node.addService(pg.Execute(shell="bash", command=config_cmd))
     node.addService(pg.Execute(shell="bash", command=run_cmds[idx]))
 
-# ---------- Output ----------
+# ---------- output ----------
 pc.printRequestRSpec(request)

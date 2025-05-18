782f1804245bfe12a1c0199c1059012885de964d
A1ex-Fu <alex.aj.fu@gmail.com>
2025-05-17 18:58:22 -0700

    lan ver

--- profile.py
+++ profile.py
@@ -1,82 +1,79 @@
+#
+# CloudLab profile: 4 nodes on a shared LAN running vrpaxos.
+# Compatible with Python 2.7 (no f‑strings, no type hints).
+#
 import geni.portal as portal
 import geni.rspec.pg as pg
-import geni.rspec.emulab as emulab
 
-IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
-NETMASK = "255.255.255.0"
-BASE_IP = "10.10.1."
-REPO = "https://github.com/A1ex-Fu/vrpaxos.git"
-DIR = "/local/repository/vrpaxos"
+pc      = portal.Context()
+request = pc.makeRequestRSpec()
 
-pc = portal.Context()
-rspec = pc.makeRequestRSpec()
+# ---------- Parameters ----------
+pc.defineParameter("phystype",  "Physical node type",
+                   portal.ParameterType.STRING, "",
+                   longDescription="Leave blank for any available type.")
 
-pc.defineParameter("phystype", "Switch type",
-                   portal.ParameterType.STRING, "dell-s4048",
-                   [("mlnx-sn2410", "Mellanox"), ("dell-s4048", "Dell")])
-pc.defineParameter("bandwidth", "Link bandwidth Mbps",
-                   portal.ParameterType.INTEGER, 1000)
-pc.defineParameter("latency", "Link latency ms",
-                   portal.ParameterType.INTEGER, 0)
+pc.defineParameter("linkSpeed", "LAN link speed",
+                   portal.ParameterType.INTEGER, 1000000,
+                   [(100000,  "100 Mb/s"),
+                    (1000000, "1 Gb/s"),
+                    (10000000,"10 Gb/s")])
+
 params = pc.bindParameters()
 
-switch = emulab.Switch("switch")
-switch.hardware_type = params.phystype
+# ---------- Basic constants ----------
+NODE_COUNT   = 4
+BASE_IP      = "10.10.1."
+PORT_START   = 8080
+DISK_IMAGE   = ("urn:publicid:IDN+emulab.net+image+emulab-ops//"
+                "UBUNTU22-64-STD")
+GIT_URL      = "https://github.com/A1ex-Fu/vrpaxos.git"
+
+# ---------- LAN ----------
+lan = request.LAN("lan0")
+lan.bandwidth = params.linkSpeed
 
 nodes = []
-switch_ifs = []
+ips   = []
 
-for i in range(4):
-    node = rspec.RawPC("node{}".format(i))
-    node.hardware_type = "xl170"
-    node.disk_image = IMG
-    iface = node.addInterface()
-    ip_addr = "{}{}".format(BASE_IP, i + 1)
-    iface.addAddress(pg.IPv4Address(ip_addr, NETMASK))
+# ---------- Create nodes ----------
+for i in range(NODE_COUNT):
+    node = request.RawPC("node%d" % i)
+    node.disk_image = DISK_IMAGE
+    if params.phystype:
+        node.hardware_type = params.phystype
+
+    # interface + IP
+    iface = node.addInterface("eth1")
+    ip    = "%s%d" % (BASE_IP, i + 2)   # 10.10.1.2, .3, …
+    iface.addAddress(pg.IPv4Address(ip, "255.255.255.0"))
+    lan.addInterface(iface)
+
     nodes.append(node)
+    ips.append(ip)
 
-    sw_iface = switch.addInterface()
-    switch_ifs.append(sw_iface)
+# ---------- Build testConfig2.txt ----------
+config_lines = ["f 0"]
+for idx, ip in enumerate(ips):
+    config_lines.append("replica %s:%d" % (ip, PORT_START + idx))
+config_text = "\\n".join(config_lines)
 
-    # Create L1Link between node interface and switch interface
-    link = rspec.L1Link("link{}".format(i))
-    link.addInterface(iface)
-    link.addInterface(sw_iface)
+# ---------- Common bootstrap for each node ----------
+clone_cmd   = "git clone {} ~/vrpaxos".format(GIT_URL)
+config_cmd  = "echo -e \"{}\" > ~/testConfig2.txt".format(config_text)
 
-    # Add bandwidth and latency shaping on this link
-    link.bandwidth = params.bandwidth  # Mbps
-    link.latency = params.latency       # ms
+# Node‑specific run commands
+run_cmds = [
+    "cd ~/vrpaxos && ./bench/replica -c ~/testConfig2.txt -i 0 -m vr",
+    "cd ~/vrpaxos && ./bench/replica -c ~/testConfig2.txt -i 1 -w -m vr",
+    "cd ~/vrpaxos && ./bench/replica -c ~/testConfig2.txt -i 2 -m vr",
+    "cd ~/vrpaxos && ./bench/client  -c ~/testConfig2.txt -m vr -n 1000 -t 1 -w 5 -l latency.txt"
+]
 
-def add_node_script(idx, node):
-    config_lines = [
-        "f 0",
-        "replica {}2:8080".format(BASE_IP),
-        "replica {}3:8081".format(BASE_IP),
-        "replica {}4:8082".format(BASE_IP)
-    ]
-    config_str = "\\n".join(config_lines)
+for idx, node in enumerate(nodes):
+    node.addService(pg.Execute(shell="bash", command=clone_cmd))
+    node.addService(pg.Execute(shell="bash", command=config_cmd))
+    node.addService(pg.Execute(shell="bash", command=run_cmds[idx]))
 
-    if idx == 0:
-        cmd = "./bench/replica -c ./testConfig2.txt -i 0 -m vr"
-    elif idx == 1:
-        cmd = "./bench/replica -c ./testConfig2.txt -i 1 -w -m vr"
-    elif idx == 2:
-        cmd = "./bench/replica -c ./testConfig2.txt -i 2 -m vr"
-    else:
-        cmd = "./bench/client -c ./testConfig2.txt -m vr -n 1000 -t 1 -w 5 -l latency.txt"
-
-    script = (
-        "sudo apt-get update && "
-        "sudo apt-get install -y git build-essential && "
-        "git clone {repo} {dir} && "
-        "echo -e \"{config}\" > {dir}/testConfig2.txt && "
-        "cd {dir} && "
-        "{cmd}"
-    ).format(repo=REPO, dir=DIR, config=config_str, cmd=cmd)
-
-    node.addService(pg.Execute(shell="bash", command=script))
-
-for i, node in enumerate(nodes):
-    add_node_script(i, node)
-
-pc.printRequestRSpec(rspec)
+# ---------- Output ----------
+pc.printRequestRSpec(request)

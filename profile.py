f968b51e7046b71bc7f0b29516bf59ee46f863f1
A1ex-Fu <alex.aj.fu@gmail.com>
2025-05-17 18:25:17 -0700

    fix typo again

--- profile.py
+++ profile.py
@@ -1,59 +1,61 @@
-# -*- coding: utf-8 -*-
 import geni.portal as portal
 import geni.rspec.pg as pg
 
-IMG      = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
-NETMASK  = "255.255.255.0"
-BASE_IP  = "10.10.1."
-NODE_HW  = "xl170"
-SW_DEF   = "dell-s4048"
-REPO_URL = "https://github.com/A1ex-Fu/vrpaxos.git"
-REPO_DIR = "/local/repository/vrpaxos"
+IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+NETMASK = "255.255.255.0"
+BASE_IP = "10.10.1."
+REPO = "https://github.com/A1ex-Fu/vrpaxos.git"
+DIR = "/local/repository/vrpaxos"
 
 pc = portal.Context()
 rspec = pc.makeRequestRSpec()
+pc.defineParameter("phystype", "Switch type",
+                   portal.ParameterType.STRING, "dell-s4048",
+                   [("mlnx-sn2410", "Mellanox"), ("dell-s4048", "Dell")])
+params = pc.bindParameters()
 
-pc.defineParameter(
-    "phystype", "Switch type",
-    portal.ParameterType.STRING, SW_DEF,
-    [("mlnx-sn2410","Mellanox SN2410"),("dell-s4048","Dell S4048")]
-)
-p = pc.bindParameters()
+switch = pg.Switch("switch")
+switch.hardware_type = params.phystype
+switch_ifs = [switch.addInterface() for _ in range(4)]
 
-sw = rspec.Switch("sw")
-sw.hardware_type = p.phystype
-swif = [sw.addInterface() for _ in range(4)]
+def add_node(idx, name, cmd):
+    node = rspec.RawPC(name)
+    node.hardware_type = "xl170"
+    node.disk_image = IMG
+    iface = node.addInterface()
+    ip_addr = "{}{}".format(BASE_IP, idx + 1)
+    iface.addAddress(pg.IPv4Address(ip_addr, NETMASK))
 
-def node(idx, name, cmd):
-    n = rspec.RawPC(name)
-    n.hardware_type = NODE_HW
-    n.disk_image = IMG
-    iface = n.addInterface()
-    iface.addAddress(pg.IPv4Address("{}{}".format(BASE_IP,idx+1), NETMASK))
-    ln = rspec.L1Link("l{}".format(idx))
-    ln.addInterface(iface)
-    ln.addInterface(swif[idx])
+    link = rspec.L1Link("link{}".format(idx))
+    link.addInterface(iface)
+    link.addInterface(switch_ifs[idx])
 
-    cfg = "\\n".join([
+    # Prepare testConfig2.txt content with correct IPs
+    config_lines = [
         "f 0",
         "replica {}2:8080".format(BASE_IP),
         "replica {}3:8081".format(BASE_IP),
         "replica {}4:8082".format(BASE_IP)
-    ])
+    ]
+    config_str = "\\n".join(config_lines)
 
-    setup = (
-        "sudo apt -y update && "
-        "sudo apt -y install git build-essential && "
-        "git clone {0} {1} && "
-        "echo -e \"{2}\" > {1}/testConfig2.txt && "
-        "cd {1} && {3}"
-    ).format(REPO_URL, REPO_DIR, cfg, cmd)
+    # Compose bash commands: install deps, clone repo, write config, run command
+    script = (
+        "sudo apt-get update && "
+        "sudo apt-get install -y git build-essential && "
+        "git clone {repo} {dir} && "
+        "echo -e \"{config}\" > {dir}/testConfig2.txt && "
+        "cd {dir} && "
+        "{cmd}"
+    ).format(repo=REPO, dir=DIR, config=config_str, cmd=cmd)
 
-    n.addService(pg.Execute(shell="bash", command=setup))
+    node.addService(pg.Execute(shell="bash", command=script))
 
-node(0,"node0","./bench/replica -c ./testConfig2.txt -i 0 -m vr")
-node(1,"node1","./bench/replica -c ./testConfig2.txt -i 1 -w -m vr")
-node(2,"node2","./bench/replica -c ./testConfig2.txt -i 2 -m vr")
-node(3,"node3","./bench/client  -c ./testConfig2.txt -m vr -n 1000 -t 1 -w 5 -l latency.txt")
+
+# Add nodes: replicas and client with witness on node1 (index 1)
+add_node(0, "node0", "./bench/replica -c ./testConfig2.txt -i 0 -m vr")
+add_node(1, "node1", "./bench/replica -c ./testConfig2.txt -i 1 -w -m vr")  # witness with -w flag
+add_node(2, "node2", "./bench/replica -c ./testConfig2.txt -i 2 -m vr")
+add_node(3, "node3", "./bench/client  -c ./testConfig2.txt -m vr -n 1000 -t 1 -w 5 -l latency.txt")
 
 pc.printRequestRSpec(rspec)

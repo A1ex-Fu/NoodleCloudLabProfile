77e11de611f55cffe1575b8b83f35ab694c50d80
A1ex-Fu <alex.aj.fu@gmail.com>
2025-05-18 14:18:45 -0700

    cd  into

--- setup-client.sh
+++ setup-client.sh
@@ -5,6 +5,7 @@ exec > /local/run.out 2>&1
 apt-get update -y
 apt-get install -y git build-essential
 git clone https://github.com/A1ex-Fu/vrpaxos.git /local/vrpaxos
+cd vrpaxos
 cat >/local/vrpaxos/testConfig2.txt <<EOF
 f 0
 replica 10.10.1.2:8080

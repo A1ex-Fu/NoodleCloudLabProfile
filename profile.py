1a21e00e1d46a30e68d2b3a439ebe8fe05fb9598
A1ex-Fu <alex.aj.fu@gmail.com>
2025-05-17 18:27:39 -0700

    fix typo

--- profile.py
+++ profile.py
@@ -1,5 +1,6 @@
 import geni.portal as portal
 import geni.rspec.pg as pg
+import geni.rspec.emulab as emulab
 
 IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 NETMASK = "255.255.255.0"
@@ -9,14 +10,17 @@ DIR = "/local/repository/vrpaxos"
 
 pc = portal.Context()
 rspec = pc.makeRequestRSpec()
+
 pc.defineParameter("phystype", "Switch type",
                    portal.ParameterType.STRING, "dell-s4048",
                    [("mlnx-sn2410", "Mellanox"), ("dell-s4048", "Dell")])
 params = pc.bindParameters()
 
-switch = pg.Switch("switch")
+switch = emulab.Switch("switch")
 switch.hardware_type = params.phystype
+
 switch_ifs = [switch.addInterface() for _ in range(4)]
+
 
 def add_node(idx, name, cmd):
     node = rspec.RawPC(name)
@@ -30,7 +34,6 @@ def add_node(idx, name, cmd):
     link.addInterface(iface)
     link.addInterface(switch_ifs[idx])
 
-    # Prepare testConfig2.txt content with correct IPs
     config_lines = [
         "f 0",
         "replica {}2:8080".format(BASE_IP),
@@ -39,7 +42,6 @@ def add_node(idx, name, cmd):
     ]
     config_str = "\\n".join(config_lines)
 
-    # Compose bash commands: install deps, clone repo, write config, run command
     script = (
         "sudo apt-get update && "
         "sudo apt-get install -y git build-essential && "
@@ -52,9 +54,8 @@ def add_node(idx, name, cmd):
     node.addService(pg.Execute(shell="bash", command=script))
 
 
-# Add nodes: replicas and client with witness on node1 (index 1)
 add_node(0, "node0", "./bench/replica -c ./testConfig2.txt -i 0 -m vr")
-add_node(1, "node1", "./bench/replica -c ./testConfig2.txt -i 1 -w -m vr")  # witness with -w flag
+add_node(1, "node1", "./bench/replica -c ./testConfig2.txt -i 1 -w -m vr")
 add_node(2, "node2", "./bench/replica -c ./testConfig2.txt -i 2 -m vr")
 add_node(3, "node3", "./bench/client  -c ./testConfig2.txt -m vr -n 1000 -t 1 -w 5 -l latency.txt")
 
